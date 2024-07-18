f658856a442548475218dd815133f90a27bfb066
Selçuk Çukur <selcukcukur@outlook.com.tr>
2024-07-18 07:53:16 +0300

    Add contextual colors to predefined settings.

--- source/theme.ts
+++ source/theme.ts
@@ -1,7 +1,31 @@
+import levdwireColors from './color'
+
 /**
  * Default theme for levdwire preset.
  */
 export const defaultTheme = {
-    fontFamily  : {},
-    extend      : {},
+    fontFamily: {},
+    extend: {
+        extend: {
+            // Levdwire :: Colors
+            colors: {
+                // Levdwire :: Color (Primary)
+                primary: levdwireColors.primary,
+                // Levdwire :: Color (Secondary)
+                secondary: levdwireColors.secondary,
+                // Levdwire :: Color (Light)
+                light: levdwireColors.light,
+                // Levdwire :: Color (Muted)
+                muted: levdwireColors.muted,
+                // Levdwire :: Color (Success)
+                success: levdwireColors.success,
+                // Levdwire :: Color (Info)
+                info: levdwireColors.info,
+                // Levdwire :: Color (Danger)
+                danger: levdwireColors.danger,
+                // Levdwire :: Color (Warning)
+                warning: levdwireColors.warning
+            },
+        },
+    }
 }
